5ffaad4d67e5fa699dba07661e43ee4b6f337a9a
Erich Gerhard Ess <erich.g.ess@gmail.com>
2019-11-23 13:04:25 -0600

    moved the tokenizer to be a part of the interpreter.  This will insure consistency in operators being used for tokenization and for interpretation.

--- main.go
+++ main.go
@@ -7,12 +7,9 @@ import (
 
 func main() {
 	input := "(3 * 4 + 2)/2"
-	tokenizer := tok.NewTokenizer([]string{"*", "+", "/"})
-	tokens := tokenizer.Tokenize(input)
-	fmt.Printf("%+v\n", tokens)
 	interpreter := tok.NewInterpreter()
 	interpreter.AddExpressionOp("+", func(a, b int) int { return a + b })
 	interpreter.AddFactorOp("*", func(a, b int) int { return a * b })
 	interpreter.AddFactorOp("/", func(a, b int) int { return a / b })
-	fmt.Println(interpreter.Execute(tokens))
+	fmt.Println(interpreter.Execute(input))
 }
--- tok/interpret.go
+++ tok/interpret.go
@@ -24,19 +24,45 @@ func NewInterpreter() Interpreter {
 }
 
 func (i *Interpreter) AddExpressionOp(symbol string, apply BinaryOperator) {
+	// make sure the operator does not exist in the Factor set
+	if _, ok := i.factorOps[symbol]; ok {
+		panic("attempting to add operator to expression set when it is already in factor set")
+	}
 	i.expOps[symbol] = apply
 }
 
 func (i *Interpreter) AddFactorOp(symbol string, apply BinaryOperator) {
+	// make sure the operator does not exist in the Expression set
+	if _, ok := i.expOps[symbol]; ok {
+		panic("attempting to add operator to factor set when it is already in expression set")
+	}
 	i.factorOps[symbol] = apply
 }
 
-func (i *Interpreter) Execute(tokens []Token) int {
+func (i *Interpreter) Execute(text string) int {
+	// construct a tokenizer
+	tokenizer := i.createTokenizer()
+
+	tokens := tokenizer.Tokenize(text)
+
 	result, pos := i.Expression(tokens, 0)
 	if pos != len(tokens) {
 		panic("unexpected tokens in expression")
 	}
 	return result
+}
+
+func (i *Interpreter) createTokenizer() Tokenizer {
+	// build a list of the operators in this interpreter
+	opsList := make([]string, 0, len(i.expOps)+len(i.factorOps))
+	for k, _ := range i.expOps {
+		opsList = append(opsList, k)
+	}
+	for k, _ := range i.factorOps {
+		opsList = append(opsList, k)
+	}
+	// create tokenizer
+	return NewTokenizer(opsList)
 }
 
 func (i *Interpreter) Expression(tokens []Token, currentPos int) (result int, pos int) {
