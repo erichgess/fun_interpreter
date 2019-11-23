7b97e4f3a0f8820425b260564d9ea7c9f7594106
Erich Gerhard Ess <erich.g.ess@gmail.com>
2019-11-23 12:44:47 -0600

    make the available operations expandable

--- main.go
+++ main.go
@@ -11,5 +11,7 @@ func main() {
 	tokens := tokenizer.Tokenize(input)
 	fmt.Printf("%+v\n", tokens)
 	interpreter := tok.NewInterpreter()
+	interpreter.AddExpressionOp("+", func(a, b int) int { return a + b })
+	interpreter.AddFactorOp("*", func(a, b int) int { return a * b })
 	fmt.Println(interpreter.Expression(tokens, 0))
 }
--- tok/interpret.go
+++ tok/interpret.go
@@ -9,26 +9,39 @@ Factor := Integer [MULTIPLY Factor]|'(' Expression ')'
 Integer := Digit+
 */
 
-type BinaryOperator interface {
-	Apply(a, b int) int
-}
+type BinaryOperator func(a, b int) int
 
 type Interpreter struct {
+	expOps    map[string]BinaryOperator
+	factorOps map[string]BinaryOperator
 }
 
 func NewInterpreter() Interpreter {
-	return Interpreter{}
+	return Interpreter{
+		expOps:    make(map[string]BinaryOperator),
+		factorOps: make(map[string]BinaryOperator),
+	}
+}
+
+func (i *Interpreter) AddExpressionOp(symbol string, apply BinaryOperator) {
+	i.expOps[symbol] = apply
+}
+
+func (i *Interpreter) AddFactorOp(symbol string, apply BinaryOperator) {
+	i.factorOps[symbol] = apply
 }
 
 func (i *Interpreter) Expression(tokens []Token, currentPos int) (result int, pos int) {
 	result, pos = i.Factor(tokens, currentPos)
 
 	if pos < len(tokens) {
-		if tokens[pos].ty == OperatorType && tokens[pos].value == "+" {
-			pos++
-			r, p := i.Expression(tokens, pos)
-			result += r
-			pos = p
+		if tokens[pos].ty == OperatorType {
+			if op, ok := i.expOps[tokens[pos].value]; ok {
+				pos++
+				r, p := i.Expression(tokens, pos)
+				result = op(result, r)
+				pos = p
+			}
 		}
 	}
 
@@ -51,11 +64,13 @@ func (i *Interpreter) Factor(tokens []Token, currentPos int) (result int, pos in
 	}
 
 	if currentPos < len(tokens) {
-		if tokens[currentPos].ty == OperatorType && tokens[currentPos].value == "*" {
-			currentPos++
-			r, p := i.Factor(tokens, currentPos)
-			result *= r
-			currentPos = p
+		if tokens[currentPos].ty == OperatorType {
+			if op, ok := i.factorOps[tokens[currentPos].value]; ok {
+				currentPos++
+				r, p := i.Factor(tokens, currentPos)
+				result = op(result, r)
+				currentPos = p
+			}
 		}
 	}
 
