e50962b592162ae6af4fa64d1e7935518782990b
Erich Gerhard Ess <erich.g.ess@gmail.com>
2019-11-23 13:35:34 -0600

    documentation

--- tok/interpret.go
+++ tok/interpret.go
@@ -10,9 +10,25 @@ Term := Integer | UnaryOp Term | LParen Expression RParen
 Integer := Digit+
 */
 
+// BinaryOperator is a function which takes two integers and returns one
 type BinaryOperator func(a, b int) int
+
+// UnaryOperator is a function which takes one integer and returns one
 type UnaryOperator func(a int) int
 
+// Interpreter allows a user to define a set of operators that fit within the following grammar
+// and then use the interpreter to compute the results of programs written in that language.
+//
+// Grammar:
+//
+// - Expression := Factor[ExpOp Expression]
+//
+// - Factor := Term [FactorOp Factor]
+//
+// - Term := Integer | UnaryOp Term | LParen Expression RParen
+//
+// - Integer := Digit+
+//
 type Interpreter struct {
 	expOps    map[string]BinaryOperator
 	factorOps map[string]BinaryOperator
@@ -27,6 +43,10 @@ func NewInterpreter() Interpreter {
 	}
 }
 
+// AddExpressionOp will add a new expression level operator for the interpreter to use.
+// If an operator already exists at the expression level with the given symbol, it will
+// be replaced.  If an operator with this symbol exists in the Factor operator set
+// then this will fail.
 func (i *Interpreter) AddExpressionOp(symbol string, apply BinaryOperator) {
 	// make sure the operator does not exist in the Factor set
 	if _, ok := i.factorOps[symbol]; ok {
@@ -35,6 +55,10 @@ func (i *Interpreter) AddExpressionOp(symbol string, apply BinaryOperator) {
 	i.expOps[symbol] = apply
 }
 
+// AddFactorOp will add a new operator with the given symbol to the Factor level
+// of interpretation.  If an operator already exists with this symbol for Factor level
+// it will be replaced.  If an operator with this symbol exists in the Expression operator
+// set then this will fail.
 func (i *Interpreter) AddFactorOp(symbol string, apply BinaryOperator) {
 	// make sure the operator does not exist in the Expression set
 	if _, ok := i.expOps[symbol]; ok {
@@ -43,10 +67,14 @@ func (i *Interpreter) AddFactorOp(symbol string, apply BinaryOperator) {
 	i.factorOps[symbol] = apply
 }
 
+// AddUnaryOp will add a unary operator that will be applied at the Term level
+// of the language.
 func (i *Interpreter) AddUnaryOp(symbol string, apply UnaryOperator) {
 	i.unaryOps[symbol] = apply
 }
 
+// Execute will take a program that uses the interpreters defined language
+// and attempt to compute it's result
 func (i *Interpreter) Execute(text string) int {
 	// construct a tokenizer
 	tokenizer := i.createTokenizer()
@@ -63,10 +91,10 @@ func (i *Interpreter) Execute(text string) int {
 func (i *Interpreter) createTokenizer() tokenizer {
 	// build a list of the operators in this interpreter
 	opsList := make([]string, 0, len(i.expOps)+len(i.factorOps))
-	for k, _ := range i.expOps {
+	for k := range i.expOps {
 		opsList = append(opsList, k)
 	}
-	for k, _ := range i.factorOps {
+	for k := range i.factorOps {
 		opsList = append(opsList, k)
 	}
 	// create tokenizer
