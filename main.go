5898ff7cd49eea0c7868872640db39953187493f
Erich Gerhard Ess <erich.g.ess@gmail.com>
2019-11-23 13:05:34 -0600

    testing

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
-	input := "(3 * 4 + 2)/2"
+	input := "(3 * 4 - 2)/2"
 	interpreter := tok.NewInterpreter()
 	interpreter.AddExpressionOp("+", func(a, b int) int { return a + b })
+	interpreter.AddExpressionOp("-", func(a, b int) int { return a - b })
 	interpreter.AddFactorOp("*", func(a, b int) int { return a * b })
 	interpreter.AddFactorOp("/", func(a, b int) int { return a / b })
 	fmt.Println(interpreter.Execute(input))
